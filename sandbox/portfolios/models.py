57d75da38d56b1a18272a17c680d78162ba886e8
Viraj Ranjankumar <viraj.ranjankumar@gmail.com>
2016-04-08 19:06:38 +0800

    Improved API access

--- sandbox/portfolios/models.py
+++ sandbox/portfolios/models.py
@@ -6,15 +6,18 @@ ASSET_CLASSES = (
 	('stock', 'Stock'), ('index', 'Index'), (None, 'Other')
 )
 class  Investment(models.Model):
-	symbol = models.CharField(max_length=5)
+	symbol = models.CharField(max_length=25)
 	name = models.CharField(max_length=50)
 	market = models.CharField(max_length=50, blank=True, null=True)
-	asset_class = models.CharField(max_length=10, blank=True, null=True, choices=ASSET_CLASSES)
-	investments = models.ManyToManyField('self', null=True, blank=True, symmetrical=False, through="Allocation")
+	asset_class = models.CharField(max_length=25, blank=True, null=True, choices=ASSET_CLASSES)
+	investments = models.ManyToManyField('self', blank=True, symmetrical=False, through="Allocation")
 
 	def __str__(self):
 		return "%s-%s" % (self.symbol, self.name)
-	
+
+	class Meta:
+		unique_together = (('symbol', 'name'),)
+
 class Allocation(models.Model):
 	portfolio = models.ForeignKey(Investment, related_name="portfolio")
 	asset = models.ForeignKey(Investment, related_name="asset", null=True, blank=True)
@@ -27,3 +30,4 @@ class HistoricPrice(models.Model):
 
 	class Meta:
 		unique_together = (('investment', 'date'),)
+		get_latest_by = "date"
--- sandbox/portfolios/views.py
+++ sandbox/portfolios/views.py
@@ -7,6 +7,7 @@ import json
 from django.db.models import Q
 from django.views.generic import ListView, View
 from django.http import JsonResponse
+from django.core.exceptions import ValidationError
 from portfolios.models import Investment, Allocation, HistoricPrice
 from portfolios.forms import DateFilterForm
 
@@ -18,14 +19,14 @@ class InvestmentList(View):
     if not form.is_valid():
       historicprice = HistoricPrice.objects.filter(investment__symbol__iexact=symbol)
       if historicprice.exists():
-        start = historicprice.first().date
         finish = historicprice.last().date
+        start = historicprice.first().date
       else:
         finish = datetime.date.today()
         start = finish - delta
     else:
-      start = form.cleaned_data['start']
       finish = form.cleaned_data['finish']
+      start = form.cleaned_data['start']
       symbol = form.cleaned_data['symbol']
 
     start_plus = start + delta
@@ -36,20 +37,27 @@ class InvestmentList(View):
     investment = Investment.objects.get(symbol__iexact=symbol)
     if investment:
       prices = HistoricPrice.objects.filter(investment=investment)
+
       if prices.filter(Q(date__range=[start_minus, start_plus])|Q(date__range=[finish_minus, finish_plus])).exists():
         objects = prices.filter(date__range=[start, finish])
         json_response = map(ast.literal_eval, objects.values_list('price', flat=True))
-        print(json_response)
       else:        
         url = 'http://real-chart.finance.yahoo.com/table.csv?%s' % urllib.urlencode({
           'a':(start.month - 1), 'b':start.day,'c':start.year,
           'd':(finish.month - 1), 'e':finish.day, 'f':finish.year, 
           's':symbol, 'g': 'd', 'ignore':'.csv' })
         print("Fetching from %s" % url)
-        remote_response = urllib2.urlopen(url)
-        cr = csv.DictReader(remote_response)
-        json_response = [row for row in cr]
-        HistoricPrice.objects.bulk_create([HistoricPrice(date=price['Date'], price=price, investment=investment) for price in json_response])
+        response = csv.DictReader(urllib2.urlopen(url))
+        json_response = [row for row in response]
+        models = []
+        for price in json_response:
+          model = HistoricPrice(date=price['Date'], price=price, investment=investment)
+          try:
+            model.full_clean()
+            models.append(model)
+          except ValidationError as e:
+            pass
+        HistoricPrice.objects.bulk_create(models)
 
     return JsonResponse(json_response, safe=False)
 
@@ -66,20 +74,25 @@ class InvestmentSearchList(View):
     if investments.exists():
       json_response = [i for i in investments.values('symbol', 'name')]
     else:
-      url = 'http://d.yimg.com/aq/autoc?query=%s&region=US&lang=en-US' % query
+      url = 'http://d.yimg.com/aq/autoc?%s' % urllib.urlencode({'query': query, 'region':'US', 'lang':'en-US'});
       print('Fetching list from %s' % url)
-      remote_response = urllib2.urlopen(url)
-      cr = json.load(remote_response)
-      investments = []
-      for i in cr['ResultSet']['Result']:
+      response = json.load(urllib2.urlopen(url))
+      models = []
+      for i in response['ResultSet']['Result']:
         if asset_type(i['typeDisp']):
-          investment = Investment(symbol=i['symbol'], name=i['name'], market=i['exch'], asset_class=asset_type(i['typeDisp']))
-          investments.append(investment)
-      Investment.objects.bulk_create(investments)
+          model = Investment(symbol=i['symbol'], name=i['name'], market=i['exch'], asset_class=asset_type(i['typeDisp']))
+          try:
+            model.full_clean()
+            models.append(model)
+          except ValidationError as e:
+            pass
+      Investment.objects.bulk_create(models)
 
-      json_response = [{'name': a['name'], 'symbol': a['symbol']} for a in cr['ResultSet']['Result']]
+      json_response = [{'name': a['name'], 'symbol': a['symbol']} for a in response['ResultSet']['Result']]
 
-    return JsonResponse(json_response, safe=False)
+    response = JsonResponse(json_response[:5], safe=False)
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
 
 def asset_type(a):
   if a == "Equity":
